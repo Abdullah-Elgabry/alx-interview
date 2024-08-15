1287cab83c9fef38e5149edd10518f85fb2b6c20
“abdullah <abdullah.h.elgabry@gmail.com>
2024-08-15 20:54:06 +0300

    parsing task

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -6,10 +6,19 @@ import datetime
 
 for i in range(10000):
     sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
+    ip_address = "{:d}.{:d}.{:d}.{:d}".format(
+        random.randint(1, 255),
+        random.randint(1, 255),
+        random.randint(1, 255),
+        random.randint(1, 255)
+    )
+    timestamp = datetime.datetime.now()
+    status_code = random.choice([200, 301, 400, 401, 403, 404, 405, 500])
+    response_size = random.randint(1, 1024)
+
+    log_entry = (
+        f"{ip_address} - [{timestamp}] "
+        f"\"GET /projects/260 HTTP/1.1\" {status_code} {response_size}\n"
+    )
+    sys.stdout.write(log_entry)
     sys.stdout.flush()
