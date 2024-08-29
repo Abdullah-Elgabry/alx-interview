ce497830715dcc2900e2f56a4d61d51ca911a045
“abdullah <abdullah.h.elgabry@gmail.com>
2024-08-29 21:10:29 +0300

    Nquee problem

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,35 +1,35 @@
 #!/usr/bin/python3
-""" NQuee task  N×N chessboard ##solved"""
+""" NQuee task  NxN chessboard ##solved """
 
 from sys import argv
 
 
 def is_valid_queen_placement(queens_positions: list) -> bool:
     """Check if the latest queen placement is valid.
-    
+
     Args:
-        queens_positions (list): List containing the column positions of queens 
-                                 placed on the board so far.
-    
+        queens_positions (list): List containing the column positions of queens
+        placed on the board so far.
+
     Returns:
         bool: True if the current queen placement is valid, False otherwise.
     """
-    current_row = len(queens_positions) - 1
-    for row in range(current_row):
-        column_difference = abs(queens_positions[row] - queens_positions[current_row])
-        if column_difference == 0 or column_difference == current_row - row:
+    n_r = len(queens_positions) - 1
+    for row in range(n_r):
+        column_difference = abs(queens_positions[row] - queens_positions[n_r])
+        if column_difference == 0 or column_difference == n_r - row:
             return False
     return True
 
 
 def solve_n_queens(n: int, row: int, queens_positions: list, solutions: list):
     """Solve the N-Queens problem recursively and print each valid solution.
-    
+
     Args:
         n (int): The size of the chessboard (n x n).
         row (int): The current row being processed.
-        queens_positions (list): List containing the column positions of queens 
-                                 placed on the board.
+        queens_positions (list): List containing the column positions of queens
+        placed on the board.
         solutions (list): List to store the current solution being built.
     """
     if row == n:
