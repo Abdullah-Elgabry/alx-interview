1c590fe14e47bf21ed7d7eb2c062c948e8f8a572
“abdullah <abdullah.h.elgabry@gmail.com>
2024-10-01 14:17:53 +0300

    prime_game_challange

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,56 +1,40 @@
 #!/usr/bin/python3
-"""
-A game where Maria and Ben compete in prime number selection.
-"""
+"""A game where Maria and Ben compete in prime number selection."""
 
 
-def determine_winner(rounds, number_list):
+def isWinner(x, nums):
     """Determine the winner between Maria and Ben based on prime numbers.
-    
-    Args:
-        rounds (int): The number of rounds played.
-        number_list (list): A list containing the numbers for each round.
-
-    Returns:
-        str: The name of the winner, or None if there is no clear winner.
     """
-    if rounds <= 0 or number_list is None:
+    if x <= 0 or nums is None:
         return None
-    if rounds != len(number_list):
+    if x != len(nums):
         return None
 
-    ben_score = 0
-    maria_score = 0
+    ben = 0
+    maria = 0
 
-    # Create an array to mark non-prime numbers
-    sieve = [1] * (max(number_list) + 1)
-    sieve[0], sieve[1] = 0, 0  # 0 and 1 are not prime numbers
-    for current in range(2, len(sieve)):
-        eliminate_multiples(sieve, current)
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for prg in range(2, len(a)):
+        primes_del(a, prg)
 
-    # Calculate scores based on the prime counts for each number
-    for number in number_list:
-        if sum(sieve[:number + 1]) % 2 == 0:
-            ben_score += 1
+    for prg in nums:
+        if sum(a[0:prg + 1]) % 2 == 0:
+            ben += 1
         else:
-            maria_score += 1
-            
-    if ben_score > maria_score:
+            maria += 1
+    if ben > maria:
         return "Ben"
-    elif maria_score > ben_score:
+    if maria > ben:
         return "Maria"
     return None
 
 
-def eliminate_multiples(array, prime):
-    """Mark the multiples of a prime number in the list as non-prime.
-    
-    Args:
-        array (list): The list where multiples will be marked.
-        prime (int): The prime number whose multiples will be marked.
+def primes_del(mns, x):
+    """this will delete primes.
     """
-    for multiple in range(2, len(array)):
+    for prg in range(2, len(mns)):
         try:
-            array[multiple * prime] = 0  # Marking multiples as non-prime
-        except IndexError:
+            mns[prg * x] = 0
+        except (ValueError, IndexError):
             break
